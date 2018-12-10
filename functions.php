52cef5110d9e339426684c8e83a6b08ec1b9bb51
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2018-12-11 00:02:59 +0200

    Pakooreguota

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+$t_from = 0;
+$t_to = 40;
+function getFeel($t) {
+	if ($t > 30) {
+		return "karšta";
+	} else if ($t > 15) {
+		return "šilta";
+	} else if ($t >= 5 && $t <= 14) {
+		return "vėsu";
+	}
+}
+$temperatures = [];
+for ($temp=$t_from; $temp < $t_to; $temp++) { 
+	$temperatures[] = [
+		'value' => $temp,
+		'feel' => getFeel($temp),
+		'style' => (getFeel($temp) == "karšta") ? "bg-danger" : "bg-success" 
+	];
+}
---
+++ teperatura_lentele.php
@@ -0,0 +1,39 @@
+<?php include('functions.php'); ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS scope="col"-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<title>TempFeel</title>
+</head>
+<body>
+	<div class="container">
+		<h1 class="bg-info text-light p-3">TempFeel</h1>
+		<div class="row"> 
+			<div class="col">
+				<table class="table table-dark">
+					<thead>
+						<tr>
+							<th>Temperature</th>
+							<th>Feeling</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+							foreach ($temperatures as $t) {
+								echo "<tr class='". $t['style'] . "'><td>" . $t['value'] .  "</td><td>". $t['feel'] . "</td></tr>";
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
